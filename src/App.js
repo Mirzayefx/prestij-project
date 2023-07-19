e11836b3a8f1d1d5cd4f9239d7dc4843dd220f34
MansurIsa <mansur69722@gmail.com>
2023-07-19 14:24:52 +0400

    change

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 const App = () => {
   return (
-    <BrowserRouter>
+    
       <Routes>
-          <Route path='/' index element={<Home />} />
+          <Route path='/' element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    
   )
 }
 
